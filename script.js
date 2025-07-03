649287e83d40b2b67102cf8382bc5eee5cca44d1
zaidoi <zaidbadgujar7@gmail.com>
2025-07-03 20:21:51 +0530

    Binary Search

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-function findElement (arr ,target){
-    
-    for (let i = 0; i < arr.length; i++){
-        if(arr[i] === target){
-            return true
-        }
+function binarySearch (arr,target){
+  let start = 0;
+  let end = arr.length - 1;
+
+  while (start <= end){
+    let mid = Math.floor(start + end/2)
+
+    if(arr[mid] === target){
+      return true
+    }else if(arr[mid] < target){
+      start = mid + 1;
+
+    }else {
+      end = mid - 1;
     }
-        return false
-    }
-const g = findElement([1,2,3,7] ,0 )
-console.log(g);
+  }
+
+  return false
+}
+
+const r = binarySearch([3,6,9,12,15,18,21],)
+
+console.log(r);
+
