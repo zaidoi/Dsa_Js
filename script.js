8c4bee7ce82136af0b9d621cf23267de70386dca
zaidoi <zaidbadgujar7@gmail.com>
2025-07-22 19:26:36 +0530

    Roman Converter

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-function palindrome(str) {
-  let newstr = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
-  let reverseStr = [...newstr].reverse().join('')
-  if(newstr === reverseStr){
-    return true
-  }else{
-    return false
-  }
-  
-}
-
-palindrome("eye");
+function convertToRoman(num) {
+  const mappingRoman = [
+    {symbol:'M', value : 1000},
+     {symbol:'CM', value : 900},
+      {symbol:'D', value : 500},
+       {symbol:'CD', value : 400},
+        {symbol:'C', value : 100},
+         {symbol:'XC', value : 90},
+          {symbol:'L', value : 50},
+           {symbol:'XL', value : 40},
+            {symbol:'X', value : 10},
+             {symbol:'IX', value : 9},
+              {symbol:'V', value : 5},
+               {symbol:'IV', value : 4},
+                {symbol:'I', value : 1}
+ 
+  ]
+ 
+  let result = ''
+ 
+  mappingRoman.forEach((el) => {
+    while(num >= el.value){
+      result += el.symbol
+      num -= el.value;
+    }
+  })
+ 
+  return result
+ }
+ 
+ convertToRoman(36);
